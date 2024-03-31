ad13ce48c0273635c6ce4ce631e5f74e6084d442
yoo <edel1212@naver.com>
2024-03-31 22:15:29 +0900

    basic data type

---
+++ src/basicDataType.dart
@@ -0,0 +1,11 @@
+void main() {
+  // ⭐️ 기본형들 Java와 같에 모든 자료형은 Object 타입이다.
+  String name = "문자열 ''로도 가능함";
+  bool flag = true;
+  int age = 20;
+  double money = 49.13;
+
+  // ⭐️ 따라서 int와 double은 num을 상속 받기 떄문에 둘다 사용이 가능하다.
+  num x = 12;
+  x = 3.145;
+}
